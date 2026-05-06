fd236709705f53b82376bc14e79c278a39259652
Cristopher <cr.ossesg@duocuc.cl>
2026-05-06 00:00:14 -0400

    Update: rama test

--- ms-usuarios/src/main/java/cl/rednorte/ms_usuarios/repository/DeceasedRepository.java
+++ ms-usuarios/src/main/java/cl/rednorte/ms_usuarios/repository/DeceasedRepository.java
@@ -1,4 +1,4 @@
-package cl.rednorte.ms_usuarios.reposity;
+package cl.rednorte.ms_usuarios.repository;
 
 import cl.rednorte.ms_usuarios.model.Deceased;
 import org.springframework.data.jpa.repository.JpaRepository;
